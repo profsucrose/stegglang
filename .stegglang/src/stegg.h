454a2dfd5084eabce3ca0656abc55bba18dddef3
BuildTools <dr.blobbles@gmail.com>
2020-04-27 15:41:54 -0700

    Added second steggpoint

--- .stegglang/src/stegg.h
+++ .stegglang/src/stegg.h
@@ -1,13 +1,17 @@
 #include <iostream>
 
 int steggpoint = -1;
+int steggpoint2 = -1;
 
 #define startstegg int main() {
 #define stegg std::cout << "Steggy!" << std::endl;
 #define endofstegg }
 #define addsteggpoint steggpoint+=1;
 #define losesteggpoint steggpoint-=1;
+#define addsteggpoint2 steggpoint2+=1;
+#define losesteggpoint2 steggpoint2-=1;
 #define whatissteggpoint std::cout << "hello mr, stegg point is " << steggpoint << std::endl;
+#define whatissteggpoint2 std::cout << "hello mr, stegg point 2 is " << steggpoint2 << std::endl;
 #define dosteggthingfor for (int i = 0; i <
 #define times ; i++)
 #define setsteggybutton void 
