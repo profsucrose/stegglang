c002b799861127ab1d06b91f11a42d711b6ad4e4
BuildTools <dr.blobbles@gmail.com>
2020-04-27 16:08:18 -0700

    Added constants

--- .stegglang/src/stegg.h
+++ .stegglang/src/stegg.h
@@ -1,23 +1,21 @@
 #include <iostream>
 
 int steggpoint = -1;
-int steggpoint2 = -1;
 
+#define steggysets const int
+#define tobe =
+#define endsteggysetting ;
 #define startstegg int main() {
 #define stegg std::cout << "Steggy!" << std::endl;
 #define endofstegg }
 #define addsteggpoint steggpoint+=1;
 #define losesteggpoint steggpoint-=1;
-#define addsteggpoint2 steggpoint2+=1;
-#define losesteggpoint2 steggpoint2-=1;
 #define whatissteggpoint std::cout << "hello mr, stegg point is " << steggpoint << std::endl;
-#define whatissteggpoint2 std::cout << "hello mr, stegg point 2 is " << steggpoint2 << std::endl;
 #define dosteggthingfor for (int i = 0; i <
-#define times ; i++) {
+#define times ; i++)
 #define setsteggybutton void 
 #define does (){
 #define steggybuttondoesnomore }
-#define steggyloopdoesnomore }
 #define pressbutton ;
 #define stoppressingbutton ();
 #define steggysays std::cout << 
@@ -25,4 +23,3 @@ int steggpoint2 = -1;
 #define steggywilldoconditionally if (
 #define steggywillthendo ) {
 #define steggywillstopdoingaction }
-#define steggywillkeepdoingconditionally while (
